4c275509daaa9a33f79d4a0b9f81fad89607012e
Matheus Freire <matheusfreire77@gmail.com>
2022-06-27 18:19:38 +0100

    added 2 more projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,31 +2,44 @@ import React from "react";
 import anawhaki from "../public/assets/projects/anawhaki.png";
 import bibleverses from "../public/assets/projects/bibleverses.png";
 import stype from "../public/assets/projects/stype.png";
+import cryptoproject from "../public/assets/projects/cryptoproject.png";
+import portfolio from "../public/assets/projects/portfolio.png";
+
 import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full">
-      <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Projects
         </p>
-        <h2 className="py-4">What I've Built</h2>
-        <div className="grid md:grid-cols-2 gap-8">
+        <h2 className='py-4'>What I've Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
-            title="Anawhaki Landing page"
+            title='Anawhaki Landing page'
             backgroundImg={anawhaki}
-            projectUrl="/anawhaki"
+            projectUrl='/anawhaki'
           />
           <ProjectItem
-            title="Bible verses"
+            title='Bible verses'
             backgroundImg={bibleverses}
-            projectUrl="/bible"
+            projectUrl='/bible'
           />
           <ProjectItem
-            title="Speed typing Game"
+            title='Speed typing Game'
             backgroundImg={stype}
-            projectUrl="/speedtype"
+            projectUrl='/speedtype'
+          />
+          <ProjectItem
+            title='Crypto'
+            backgroundImg={cryptoproject}
+            projectUrl='/crypto'
+          />
+          <ProjectItem
+            title='Portfolio'
+            backgroundImg={portfolio}
+            projectUrl='/portfolio'
           />
         </div>
       </div>
---
+++ pages/portfolio.jsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import React from "react";
+import portfoliopic from "../public/assets/projects/portfolio.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+
+const portfolio = () => {
+  return (
+    <div className='w-full'>
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={portfoliopic}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>My portfolio</h2>
+          <h3>React JS / NextJS / Tailwind </h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            I built this portfolio to show my projects working with React JS,
+            NextJS and tailwind.
+          </p>
+
+          <button className='px-8 py-2 mt-4 mr-8'>
+            <Link href='https://fmatheus.vercel.app/' target='_blank'>
+              DEMO
+            </Link>
+          </button>
+          <button className='px-8 py-2 mt-4 mr-8'>
+            <Link href='https://github.com/fmatheus7/math-port' target='_blank'>
+              CODE
+            </Link>
+          </button>
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Tailwind
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Javascript
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Next JS
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+          <p className='underline cursor-pointer'>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default portfolio;
